e7ea447a8135a696c0c0140435eda2597f129c8d
Farahnaz <161342559+FarahnazMehrafza@users.noreply.github.com>
2024-04-04 13:59:33 -0400

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by farahnaz mehrafza on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
